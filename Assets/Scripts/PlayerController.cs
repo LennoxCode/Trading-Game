6cdcbf9233fc258c41dd2c343a714cd2988d7149
LennoxCode <leonard.valentin1999@gmail.com>
2022-04-09 18:52:31 +0200

    added values to playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,16 @@ public class PlayerController : MonoBehaviour
 {
 
     public static GameObject PInstance;
-
+    [SerializeField] private readonly int startMoney;
+    [SerializeField] private readonly int startFood;
+    private int money;
+    private int food;
     // Start is called before the first frame update
     void Start()
     {
-        PInstance = this.gameObject;    
+        PInstance = this.gameObject;
+        money = startMoney;
+        food = startFood;
     }
 
     // Update is called once per frame
