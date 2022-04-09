78e106253172349f6d9d8848b71e53ca28e61c9b
LennoxCode <leonard.valentin1999@gmail.com>
2022-04-09 19:15:04 +0200

    added scaffold class to represent inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private ArrayList itemsInInventory;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
+public class ItemInInventory
+{
+    TradingGood good;
+    int amountHeld;
+
+}
