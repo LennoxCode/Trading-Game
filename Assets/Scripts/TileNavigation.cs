b317a3cd82244ae0332e29027078195d8804354f
LennoxCode <leonard.valentin1999@gmail.com>
2022-04-16 23:30:13 +0200

    added new trade good

--- Assets/Scripts/TileNavigation.cs
+++ Assets/Scripts/TileNavigation.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class TileNavigation : MonoBehaviour
 {
     [SerializeField] private Grid mapGrid;
+    private List<Vector3> frontierPositions;
+    private List<Vector3> reachedPositions;
     public static TileNavigation instance;
+    
+    
     // Start is called before the first frame update
     private void Awake()
     {
@@ -18,6 +22,26 @@ public class TileNavigation : MonoBehaviour
     {
 
         Vector3 cellPosition = mapGrid.WorldToCell(fromPosition);
-        Debug.Log(cellPosition);
+        Vector3 target = mapGrid.WorldToCell(toPosition);
+        frontierPositions.Add(cellPosition);
+        reachedPositions.Add(cellPosition);
+        while (frontierPositions.Count > 0)
+        {
+            Vector3 current = frontierPositions[0];
+            frontierPositions.Remove(current);
+            if (current == target) break;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    Vector3 offset = new Vector3(x, y, 0);
+                    frontierPositions.Add(mapGrid.WorldToCell(current + offset));
+                    reachedPositions.Add(mapGrid.WorldToCell(current + offset));
+
+                }
+            }
+        }
+        
     }
+    
 }
