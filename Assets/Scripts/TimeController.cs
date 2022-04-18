c935f4c7d7b06447b543d6a0cc065b3254b41e1b
LennoxCode <leonard.valentin1999@gmail.com>
2022-04-18 18:47:02 +0200

    implemented time display

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    private DayTime currentTime;
+
+    public int secondsPassed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondsPassed = 0;
+        StartCoroutine(IncrementTime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime.hours = secondsPassed / 60;
+        currentTime.minutes = secondsPassed % 60;
+    }
+
+    private void LateUpdate()
+    {
+        TopBarController.instance.UpdateTimeDisplay(currentTime);
+    }
+
+    public IEnumerator IncrementTime()
+    {
+        while (true)
+        {
+            secondsPassed += 1;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+    public DayTime GetCurrentTime()
+    {
+        return currentTime;
+    }
+    public struct DayTime
+    {
+        public int hours;
+        public int minutes;
+    }
+}
---
+++ Assets/Scripts/TopBarController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TopBarController : MonoBehaviour
+{
+    public static TopBarController instance;
+
+    [SerializeField] private TextMeshProUGUI foodDisplay;
+    [SerializeField] private TextMeshProUGUI moneyDisplay;
+    [SerializeField] private TextMeshProUGUI timeDisplay;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void UpdateTimeDisplay(TimeController.DayTime currTime)
+    {
+        timeDisplay.text = currTime.hours.ToString("00") + ":" + currTime.minutes.ToString("00");
+    }
+
+    public void UpdateRessourceDisplay()
+    {
+        
+    }
+}
